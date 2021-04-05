c370a673d289422a326ef9bc67e0bdc2786ef653
Gely <mangelica.torresh@gmail.com>
2021-04-05 10:24:42 -0500

    added folders & package.json

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Desarrollo de Aplicaciones 2021',
     dest: 'public',
+    base: '/CGS2021/',
     themeConfig: {
         nav: [
           { text: 'Inicio', link: '/' }
---
+++ docs/PrincipiosDespliegue/README.md
@@ -0,0 +1,1 @@
+# Hola principios de despliegue!
